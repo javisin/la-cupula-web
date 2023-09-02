2b4100fff1c3681472b44ae24efa68f31f526946
Javi Sintes <javier@haddock.app>
2023-09-02 19:17:44 +0200

    feat: update horario image

--- src/pages/schedule/Schedule.jsx
+++ src/pages/schedule/Schedule.jsx
@@ -9,7 +9,7 @@ function Schedule() {
         <div className="container mt-3 text-center">
           <h3>Horarios</h3>
           <img className="w-100" src={schedule} alt="Horario" />
-          <a className="btn btn-secondary" href={schedule} download="horario.jpeg">Descargar horario</a>
+          <a className="mt-3 btn btn-secondary" href={schedule} download="horario.jpeg">Descargar horario</a>
         </div>
       </Page>
     </div>
