c75dbbac11935507504eb68b19cb3a7f8b329ac8
Javi Sintes <javisin98@gmail.com>
2024-06-02 21:45:31 +0200

    chore: update schedule and prices images

--- src/pages/schedule/Schedule.jsx
+++ src/pages/schedule/Schedule.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Page from '../../components/Page';
-import schedule from '../../assets/horario.jpeg';
+import schedule1 from '../../assets/horario1.jpeg';
+import schedule2 from '../../assets/horario2.jpeg';
+import schedule3 from '../../assets/horario3.jpeg';
+import schedule4 from '../../assets/horario4.jpeg';
 
 function Schedule() {
   return (
@@ -8,8 +11,13 @@ function Schedule() {
       <Page>
         <div className="container mt-3 text-center">
           <h3>Horarios</h3>
-          <img className="w-100" src={schedule} alt="Horario" />
-          <a className="mt-3 btn btn-secondary" href={schedule} download="horario.jpeg">Descargar horario</a>
+          <img className="w-50 mb-3" src={schedule1} alt="Horario" />
+          <br />
+          <img className="w-50 mb-3" src={schedule2} alt="Horario" />
+          <br />
+          <img className="w-50 mb-3" src={schedule3} alt="Horario" />
+          <br />
+          <img className="w-50 mb-3" src={schedule4} alt="Horario" />
         </div>
       </Page>
     </div>
